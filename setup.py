e3bf53b4b7a774258db8fd30f0ababc595bf962b
asatur96 <balyanasatur19@gmail.com>
2019-10-23 21:37:01 +0200

    added setup for pip

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README') as f:
+    long_description = ''.join(f.readlines())
+
+setup(
+    name='GHIA',
+    version='0.2',
+    description='GHIA: pattern-based assigning of GitHub issues',
+    author='Asatur Balyan',
+    author_email='balyaasa@fit.cvut.cz',
+    long_description=long_description,
+    license='Public Domain',
+    url='https://github.com/asatur96/ghia',
+    install_requires=['Flask', 'click>=6', 'requests'],
+    packages=['ghia'],
+)
