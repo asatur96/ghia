44d664660bb489759a87cc4dd45cc2cf4be9ecc9
asatur96 <balyanasatur19@gmail.com>
2019-11-04 02:30:34 +0100

    fixed templates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README') as f:
 
 setup(
     name='ghia-balyaasa',
-    version='0.3.2.10',
+    version='0.3.2.11',
     description='GHIA: pattern-based assigning of GitHub issues',
     author='Asatur Balyan',
     author_email='balyaasa@fit.cvut.cz',
